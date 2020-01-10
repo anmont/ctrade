759e0c335ab6d3779827e9afda3b36bd268a7745
justin <anmont@gmail.com>
2020-01-10 16:08:28 +0700

    Basic Camera controller on default scene

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    //public Camera mycam;
+    private Transform myCamera = null; 
+    private Vector3 dest;
+    private static float speed = 8f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        myCamera = GameObject.FindGameObjectWithTag("MainCamera").GetComponent<Transform>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButton("Vertical") || Input.GetButton("Horizontal") )
+        {
+            dest = new Vector3(Input.GetAxis("Horizontal"), 0.0f, Input.GetAxis("Vertical"));
+            myCamera.position = myCamera.position += (dest * speed);
+        }
+        
+        
+    }
+}
