80f0a76c335554ada2454b4d841f002e07408e20
justin <anmont@gmail.com>
2020-01-10 17:25:02 +0700

    Add NavMesh and Agent

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,18 +1,23 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class PlayerController : MonoBehaviour
 {
     //public Camera mycam;
     private Transform myCamera = null; 
     private Vector3 dest;
-    private static float speed = 8f;
+    public static float speed = 8f;
+    public Camera mainCamera;
+    public NavMeshAgent agent;
 
     // Start is called before the first frame update
     void Start()
     {
         myCamera = GameObject.FindGameObjectWithTag("MainCamera").GetComponent<Transform>();
+        mainCamera = myCamera.GetComponent<Camera>();
+        agent = GameObject.FindGameObjectWithTag("Player").GetComponent<NavMeshAgent>();
     }
 
     // Update is called once per frame
@@ -22,8 +27,18 @@ public class PlayerController : MonoBehaviour
         {
             dest = new Vector3(Input.GetAxis("Horizontal"), 0.0f, Input.GetAxis("Vertical"));
             myCamera.position = myCamera.position += (dest * speed);
+            
         }
         
-        
+        if (Input.GetMouseButtonDown(0))
+        {
+            Ray castme = mainCamera.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+
+            if (Physics.Raycast(castme, out hit))
+            {
+                    agent.SetDestination(hit.point);
+            }
+        }
     }
 }
