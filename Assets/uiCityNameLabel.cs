501275c0945ac4a679e31eb4f4a4b5eccd3f90e4
anmont <anmont@gmail.com>
2020-05-25 18:14:56 +0800

    added city labels

---
+++ Assets/uiCityNameLabel.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class uiCityNameLabel : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        this.GetComponent<Text>().text = this.transform.parent.transform.parent.transform.parent.name.ToString();
+        //Text string = this.GetComponent<Text>().text
+        //Debug.Log("debug city: " + this.transform.parent.transform.parent.transform.parent.name.ToString());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
